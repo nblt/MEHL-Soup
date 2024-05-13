e674629619dd681b36e675c32d8ca4a429f9c844
nblt <ltwtzjh@126.com>
2024-05-13 09:57:01 +0800

    update

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ dataset=ImageNet
 DST=../models/
 
 batch=18
-CUDA_VISIBLE_DEVICES=$devices python -m torch.distributed.launch --nproc_per_node 1 --master_port $port train_twa_layer_batch_dir.py \
+CUDA_VISIBLE_DEVICES=$devices python -m torch.distributed.launch --nproc_per_node 1 --master_port $port MEHL_soup_layer.py \
     --lr $lr --batch-size 128 --wd $wd --epochs 5 \
     --cosine-lr --optimizer adamw --layer-size $layer \
     --model-location $DST \
